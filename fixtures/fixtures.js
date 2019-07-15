a0f99cb4e60e276f0a0c50952fbecc8d4a44a2de
darijan92 <majicdarijan@gmail.com>
2019-07-15 13:20:22 +0200

    fixtures to arrow functions

--- fixtures/fixtures.js
+++ fixtures/fixtures.js
@@ -1,29 +1,29 @@
-const sequelize_fixtures = require('sequelize-fixtures');
+const sequelizeFixtures = require('sequelize-fixtures');
 
 const models = require('../models/index');
 
-sequelize_fixtures.loadFile('fixtures/messages.json', models)
-    .then(function () {
+sequelizeFixtures.loadFile('fixtures/messages.json', models)
+    .then(() => {
         console.log('Fixtures: messages ...');
     });
-sequelize_fixtures.loadFile('fixtures/users.json', models)
-    .then(function () {
+sequelizeFixtures.loadFile('fixtures/users.json', models)
+    .then(() => {
         console.log('Fixtures: users ...');
     });
-sequelize_fixtures.loadFile('fixtures/emoticonsGroups.json', models)
-    .then(function () {
+sequelizeFixtures.loadFile('fixtures/emoticonsGroups.json', models)
+    .then(() => {
         console.log('Fixtures: emoticonsGroups ...');
     });
-sequelize_fixtures.loadFile('fixtures/emoticons.json', models)
-    .then(function () {
+sequelizeFixtures.loadFile('fixtures/emoticons.json', models)
+    .then(() => {
         console.log('Fixtures: emoticons ...');
     });
-sequelize_fixtures.loadFile('fixtures/settings.json', models)
-    .then(function () {
+sequelizeFixtures.loadFile('fixtures/settings.json', models)
+    .then(() => {
         console.log('Fixtures: settings ...');
     });
 
-sequelize_fixtures.loadFile('fixtures/ratings.json', models)
-    .then(function () {
+sequelizeFixtures.loadFile('fixtures/ratings.json', models)
+    .then(() => {
         console.log('Fixtures: ratings ...');
     });
