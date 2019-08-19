5cd41fc46a795fd52acae582998c90c5fece73fa
darijan92 <majicdarijan@gmail.com>
2019-08-19 13:53:13 +0200

    database settings

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 require('dotenv').config('/.env');
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
 });
 
 con.connect((err) => {
